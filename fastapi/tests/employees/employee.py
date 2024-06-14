fd2fa7d14f6f5f855434e65827c832b2677376b8
ITSamantha <diana.saveleva.dos@gmail.com>
2024-06-14 21:15:11 +0300

    tests added

--- fastapi/tests/employees/employee.py
+++ fastapi/tests/employees/employee.py
@@ -1,7 +1,8 @@
 import pytest
 from httpx import AsyncClient
 
-from src.apps.employees.schemas.employee import CreateEmployee, CreateEmployeePosition, UpdateEmployeePosition
+from src.apps.employees.schemas.employee import CreateEmployee, CreateEmployeePosition, UpdateEmployeePosition, \
+    UpdateEmployee
 from tests.conftest import app, async_client, event_loop, setup_db, setup_and_teardown_db
 
 """EMPLOYEE POSITIONS"""
@@ -59,11 +60,57 @@ async def test_delete_employee_positions(anyio_backend, async_client: AsyncClien
     assert len(response.json()) == 1
 
 
+"""EMPLOYEE"""
+
+
 @pytest.mark.parametrize('anyio_backend', ['asyncio'])
 async def test_get_employees(anyio_backend, async_client: AsyncClient):
     response = await async_client.get("/employees/")
     assert response.status_code == 200
     assert len(response.json()) == 0
 
-    # employee: CreateEmployee = CreateEmployee()
-    # response = await async_client.post("/employees/", data={})
+
+@pytest.mark.parametrize('anyio_backend', ['asyncio'])
+async def test_work_employees(anyio_backend, async_client: AsyncClient):
+    instance: dict = CreateEmployee(last_name="Попов",
+                                    first_name="Поп",
+                                    patronymic=None,
+                                    login="popovpop",
+                                    password="qwerty222222",
+                                    email="popovpop@gmail.com",
+                                    unit_id=1,
+                                    position_id=2).model_dump()
+    response = await async_client.post("/employees/", json=instance)
+    assert response.status_code == 200
+
+    response = await async_client.get("/employees/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    instance: dict = CreateEmployee(last_name="Иванов",
+                                    first_name="Иван",
+                                    patronymic=None,
+                                    login="popovpop11",
+                                    email="popovpop@gmail.com",
+                                    password="qw5jsjy222222",
+                                    unit_id=1,
+                                    position_id=2).model_dump()
+
+    response = await async_client.post("/employees/", json=instance)
+    assert response.status_code == 500
+
+    response = await async_client.get("/employees/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    instance: dict = UpdateEmployee(last_name="Поляков",
+                                    first_name="Алексей",
+                                    patronymic=None,
+                                    login="popovpop",
+                                    password="qwerty222222",
+                                    email="popovpop@gmail.com",
+                                    unit_id=1,
+                                    position_id=2).model_dump()
+    response = await async_client.patch("/employees/1", json=instance)
+    assert response.status_code == 200
+    assert response.json()["last_name"] == "Поляков"
--- fastapi/tests/employees/vacation.py
+++ fastapi/tests/employees/vacation.py
@@ -1,8 +1,10 @@
+import datetime
+
 import pytest
 from httpx import AsyncClient
 
 from src.apps.employees.schemas.vacation import CreateVacationType, UpdateVacationType, UpdateVacationReason, \
-    CreateVacationReason
+    CreateVacationReason, CreateVacation, UpdateVacation
 from tests.conftest import app, async_client, event_loop, setup_db, setup_and_teardown_db
 
 """VACATION TYPES"""
@@ -93,3 +95,60 @@ async def test_delete_vacation_reasons(anyio_backend, async_client: AsyncClient)
     response = await async_client.get("/employees/vacations/reasons")
     assert response.status_code == 200
     assert len(response.json()) == 1
+
+
+"""VACATION"""
+
+
+@pytest.mark.parametrize('anyio_backend', ['asyncio'])
+async def test_get_vacations(anyio_backend, async_client: AsyncClient):
+    response = await async_client.get("/employees/vacations")
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+
+
+@pytest.mark.parametrize('anyio_backend', ['asyncio'])
+async def test_work_vacations(anyio_backend, async_client: AsyncClient):
+    instance: dict = CreateVacation(vacation_type_id=2,
+                                    vacation_reason_id=1,
+                                    comment=None,
+                                    employee_id=2,
+                                    start_date=datetime.date(2024, 12, 5),
+                                    end_date=datetime.date(2024, 12, 3)).model_dump()
+    response = await async_client.post("/employees/vacations", json=instance)
+    assert response.status_code == 422
+
+    response = await async_client.get("/employees/vacations")
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+
+    instance: dict = CreateVacation(vacation_type_id=2,
+                                    vacation_reason_id=1,
+                                    comment=None,
+                                    employee_id=2,
+                                    start_date=datetime.date(2024, 12, 3),
+                                    end_date=datetime.date(2024, 12, 5)).model_dump()
+    response = await async_client.post("/employees/vacations", json=instance)
+    assert response.status_code == 200
+
+    instance: dict = CreateVacation(vacation_type_id=2,
+                                    vacation_reason_id=1,
+                                    comment=None,
+                                    employee_id=2,
+                                    start_date=datetime.date(2024, 12, 4),
+                                    end_date=datetime.date(2024, 12, 12)).model_dump()
+    response = await async_client.post("/employees/vacations", json=instance)
+    assert response.status_code == 422
+
+    response = await async_client.get("/employees/vacations")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    instance: dict = UpdateVacation(vacation_type_id=2,
+                                    vacation_reason_id=1,
+                                    comment="Всё-таки в отпуск!",
+                                    start_date=datetime.date(2024, 12, 3),
+                                    end_date=datetime.date(2024, 12, 5)).model_dump()
+    response = await async_client.patch("/employees/vacations/1", json=instance)
+    assert response.status_code == 200
+    assert response.json()["comment"] == "Всё-таки в отпуск!"
